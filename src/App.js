6418b7639231eea7ed946f23cf60dc61180eb834
GeffersonST <83526000+GeffersonST@users.noreply.github.com>
2023-09-25 11:36:31 +0100

    Incio Projeto e Inserindo Axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+const getItem = async () => {
+
+  await axios.get("https://pokeapi.co/api/v2/pokemon");
+
+}
+
+
+getItem();
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-background">
+      <h1>Lista de Itens</h1>
+    <ul>
+      <li>Item</li>
+      <li>Item</li>
+      <li>Item</li>
+      <li>Item</li>
+      <li>Item</li>
+    </ul>
+
+      </div>
     </div>
   );
 }
