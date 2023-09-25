6ff5e44bfeed6d7bba3d9e4efb0f3d9c068b2374
GeffersonST <83526000+GeffersonST@users.noreply.github.com>
2023-09-25 12:05:53 +0100

    API desenvolvida

--- src/App.js
+++ src/App.js
@@ -1,43 +1,53 @@
-import axios from 'axios';
-import './App.css';
-import { useEffect, useState } from 'react';
+import axios from 'axios'; // Importa a biblioteca axios para fazer requisições HTTP.
+import './App.css'; // Importa um arquivo de estilo CSS.
+import { useEffect, useState } from 'react'; // Importa os hooks useEffect e useState do React.
 
 function App() {
-
-  const [itemData, setItemData] = useState([]);
+  const [pokemonData, setPokemonData] = useState([]); // Define um estado 'pokemonData' usando o hook useState, inicializado como um array vazio.
 
   useEffect(() => {
-    const getItem = async () => {
+    const fetchPokemonData = async () => {
+      try {
+        // Faz uma requisição GET para a API do Pokémon usando axios.
+        const response = await axios.get("https://pokeapi.co/api/v2/pokemon");
+        const pokemons = response.data.results;
 
-      await axios.get("https://pokeapi.co/api/v2/pokemon").then((response) => {
-        setItemData(response.data.results);
-      })
+        // Utiliza o método map para criar um array de promessas para obter os detalhes de cada Pokémon.
+        const pokemonDetails = await Promise.all(
+          pokemons.map(async (pokemon) => {
+            // Faz uma nova requisição GET para obter os detalhes de um Pokémon específico.
+            const detailsResponse = await axios.get(pokemon.url);
+            const imageUrl = detailsResponse.data.sprites.front_default;
+            // Retorna um objeto com o nome e a URL da imagem do Pokémon.
+            return { name: pokemon.name, imageUrl };
+          })
+        );
 
-    }
+        // Atualiza o estado 'pokemonData' com os detalhes dos Pokémon.
+        setPokemonData(pokemonDetails);
+      } catch (error) {
+        // Em caso de erro, exibe uma mensagem no console.
+        console.error("Erro ao buscar dados de Pokémon:", error);
+      }
+    };
 
-
-    getItem();
-  },[])
-
-
-
-
-
+    // Chama a função 'fetchPokemonData' quando o componente é montado ([] como segundo argumento).
+    fetchPokemonData();
+  }, []);
 
   return (
     <div className="App">
       <div className="App-background">
-      <h1>Lista de Itens</h1>
-    <ul>
-      <li>
-    {itemData.map((pokemon) => {
-      return <li>{pokemon.name}</li>
-    })}
-
-      </li>
-
-    </ul>
-
+        <h1>Lista de Pokémon</h1>
+        <ul>
+          {/* Mapeia os dados dos Pokémon e cria uma lista de elementos <li> para exibir cada Pokémon. */}
+          {pokemonData.map((pokemon) => (
+            <li key={pokemon.name}>
+              <img src={pokemon.imageUrl} alt={pokemon.name} />
+              {pokemon.name}
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
